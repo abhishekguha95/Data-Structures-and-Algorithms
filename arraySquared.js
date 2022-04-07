69257accc760c76c3362b0952163a74023a2a806
Abhishek Guha <qohor2525@gmail.com>
2022-04-07 16:07:12 +0530

    added anagram

--- arraySquared.js
+++ arraySquared.js
@@ -5,103 +5,106 @@
 const arrayLength = 40400;
 var arrOne = [];
 var arrSquared = [];
-for (let i=0; i < arrayLength; i++){
-  arrOne.push(i);
-  arrSquared.push(i*i);
+for (let i = 0; i < arrayLength; i++) {
+    arrOne.push(i);
+    arrSquared.push(i * i);
 }
 // console.log('arrOne: ', arrOne)
 // console.log('arrSquared: ', arrSquared)
 
 function naiveSolutionArraySquared(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i of arr1) {
-    let arr2_index = arr2.indexOf(i ** 2); // indexOf loops through entire array in worstcase
-    if (arr2_index === -1) {                // thus making this nested loop : O(N2)
-      // index of elements not present in array is -1
-      return false;
+    if (arr1.length !== arr2.length) {
+        return false;
     }
-    arr2.splice(arr2_index, 1); // removes the element at given index
-  }
-  return true;
+    for (let i of arr1) {
+        let arr2_index = arr2.indexOf(i ** 2); // indexOf loops through entire array in worstcase
+        if (arr2_index === -1) {
+            // thus making this nested loop : O(N2)
+            // index of elements not present in array is -1
+            return false;
+        }
+        arr2.splice(arr2_index, 1); // removes the element at given index
+    }
+    return true;
 }
 
 // positive case
 let t1 = performance.now();
-let naive = naiveSolutionArraySquared(arrOne, arrSquared)
+let naive = naiveSolutionArraySquared(arrOne, arrSquared);
 let t2 = performance.now();
-console.log(naive, 'time by naive: ', `${t2-t1}`);
+console.log(naive, 'time by naive: ', `${t2 - t1}`);
 
 // negative case
 var arrOne = [];
 var arrSquared = [];
-for (let i=0; i < arrayLength; i++){
-  arrOne.push(i);
-  arrSquared.push(i*i);
+for (let i = 0; i < arrayLength; i++) {
+    arrOne.push(i);
+    arrSquared.push(i * i);
 }
 //arrSquared[arrSquared.length - 1] = 1;
-arrSquared[10] = 0
+arrSquared[10] = 0;
 t1 = performance.now();
-naive = naiveSolutionArraySquared(arrOne, arrSquared)
+naive = naiveSolutionArraySquared(arrOne, arrSquared);
 t2 = performance.now();
-console.log(naive, 'time by naive: ', `${t2-t1}`);
+console.log(naive, 'time by naive: ', `${t2 - t1}`);
 
 //----------------------------------------------------------------------------------------------------------
 
 var arrOne = [];
 var arrSquared = [];
-for (let i=0; i < arrayLength; i++){
-  arrOne.push(i);
-  arrSquared.push(i*i);
+for (let i = 0; i < arrayLength; i++) {
+    arrOne.push(i);
+    arrSquared.push(i * i);
 }
 // console.log('arrOne: ', arrOne)
 // console.log('arrSquared: ', arrSquared)
 
 function optimalSolutionArraySquared(arr1, arr2) {
-  if (arr1.length !== arr2.length) {   // initial length check
-    return false;
-  }
-  let arr1_obj = {};   // using objects 
-  let arr2_obj = {};
+    if (arr1.length !== arr2.length) {
+        // initial length check
+        return false;
+    }
+    let arr1_obj = {}; // using objects
+    let arr2_obj = {};
 
-  for (let i of arr1) {                       // key is array element, value is frequency
-    arr1_obj[i] = (arr1_obj[i] || 0) + 1;
-  }
-  for (let i of arr2) {
-    arr2_obj[i] = (arr2_obj[i] || 0) + 1;
-  }
-  //console.log('arr1_obj: ', arr1_obj);
-  //console.log('arr2_obj: ', arr2_obj);
-  for (let key in arr1_obj) {
-    if(!arr2_obj[key**2]){
-      return false;
+    for (let i of arr1) {
+        // key is array element, value is frequency
+        arr1_obj[i] = (arr1_obj[i] || 0) + 1;
     }
-    if (arr2_obj[key**2] !== arr1_obj[key]) {
-      return false;
+    for (let i of arr2) {
+        arr2_obj[i] = (arr2_obj[i] || 0) + 1;
     }
-  }
-  return true;
+    //console.log('arr1_obj: ', arr1_obj);
+    //console.log('arr2_obj: ', arr2_obj);
+    for (let key in arr1_obj) {
+        if (!arr2_obj[key ** 2]) {
+            return false;
+        }
+        if (arr2_obj[key ** 2] !== arr1_obj[key]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 //positive case
 t1 = performance.now();
 let optimal = optimalSolutionArraySquared(arrOne, arrSquared);
 t2 = performance.now();
-console.log(optimal, 'time by optimal: ', `${t2-t1}`);
+console.log(optimal, 'time by optimal: ', `${t2 - t1}`);
 
 // negative case
 var arrOne = [];
 var arrSquared = [];
-for (let i=0; i < arrayLength; i++){
-  arrOne.push(i);
-  arrSquared.push(i*i);
+for (let i = 0; i < arrayLength; i++) {
+    arrOne.push(i);
+    arrSquared.push(i * i);
 }
 //arrSquared[arrSquared.length - 1] = 1;
-arrSquared[10] = 0
+arrSquared[10] = 0;
 t1 = performance.now();
-optimal = optimalSolutionArraySquared([1, 2, 5, 5], [4, 2, 1, 2])
+optimal = optimalSolutionArraySquared([1, 2, 5, 5], [4, 2, 1, 2]);
 t2 = performance.now();
-console.log(optimal, 'time by optimal: ', `${t2-t1}`);
+console.log(optimal, 'time by optimal: ', `${t2 - t1}`);
 
 //TODO: more edgecases
